4ca533972c39f112af71956e847cf43141d6c316
Leaderfirestar <EricAWebb2000@yahoo.com>
2019-11-07 10:14:12 -0600

    submitting email now pulls all emails from db and displays them

--- newapp/controllers/landing.js
+++ newapp/controllers/landing.js
@@ -1,5 +1,6 @@
 
 const models = require('../models')
+
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
@@ -9,7 +10,12 @@ exports.submit_lead = function(req, res, next) {
   return models.Lead.create({
       email: req.body.lead_email
   }).then(lead => {
-      res.redirect('/');
+      res.redirect('/leads');
   })
-  res.redirect('/');
+}
+
+exports.show_leads = function(req, res, next) {
+    models.Lead.findAll().then(leads => {
+  res.render('landing', { title: 'Express', leads: leads });
+    })
 }
