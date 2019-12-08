3dc393df072bfef3bbaba92e71a2c80e9d17ca58
Leaderfirestar <EricAWebb2000@yahoo.com>
2019-12-08 17:52:14 -0600

    Finished video, all should be well

--- newapp/controllers/user.js
+++ newapp/controllers/user.js
@@ -1,7 +1,7 @@
 let models = require("../models");
 let bcrypt = require("bcrypt");
 const passport = require('passport');
-const myPassport = require('../passport_setup)(passport);
+const myPassport = require('../passport_setup')(passport);
 let flash = require('connect-flash');
 const {validateUser} = require('../validators/signup');
 const {isEmpty} = require('lodash');
@@ -20,31 +20,58 @@ const rerender_signup = function(errors, req, res, next){
 const generateHash = function(password){
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
 }
-exports.signup = function(req, res, next){
-  let erros = {};
-  return validateUser(errors, req).then(errors =>{
-    if (!isEmpty(errors)){
-      rerender_signup(errors, req, res, next);
-    }else{
-      const newUser = models.User.build({
-        email: req.body.email,
-        password: generateHash(req.body.password)
-      });
-      return newUser.save().then(result => {
-        passport.authenticate('local', {
-          successRedirect: "/",
-          failureRedirect: "/signup"
-          failureFlash: true
-        })(req, res, next);
-      })
-    }
-  })
+exports.signup = function(req, res, next) {
+	let errors = {};
+	return validateUser(errors, req).then(errors => {
+		if (!isEmpty(errors)) {
+			rerender_signup(errors, req, res, next);
+		} else {
+			return models.User.findOne({
+				where: {
+					is_admin: true
+				}
+			}).then(user => {
+				let newUser;
+				if (user !== null) {
+					newUser = models.User.build({
+						email: req.body.email,
+						password: generateHash(req.body.password)
+					});					
+				} else {
+					newUser = models.User.build({
+						email: req.body.email,
+						password: generateHash(req.body.password),
+						is_admin: true
+					});
+				}
+				return newUser.save().then(result => {
+					passport.authenticate('local', {
+						successRedirect: "/",
+						failureRedirect: "/signup",
+						failureFlash: true
+					})(req, res, next);
+				})	
+			})		
+		}
+	})
 }
 
+exports.login = function(req, res, next) { 
+	passport.authenticate('local', {
+		successRedirect: "/",
+		failureRedirect: "/login",
+		failureFlash: true
+	})(req, res, next);
+}
+exports.logout = function(req, res, next) { 
+	req.logout();
+	req.session.destroy();
+	res.redirect('/');
+}
 exports.login = function(req, res, next){
   passport.authenticate('local', {
       successRedirect: "/",
-      failureRedirect: "/login"
+      failureRedirect: "/login",
       failureFlash: true
     })(req, res, next);
 }
@@ -52,5 +79,5 @@ exports.login = function(req, res, next){
 exports.logout = function(req, res, next){
   req.logout();
   req.session.destroy();
-  req.redirect('/');
+  res.redirect('/');
 }
--- newapp/validators/signup.js
+++ newapp/validators/signup.js
@@ -8,14 +8,14 @@ const validateCreateUserFields = function(errors, req){
     errors["password"] = "Please use Ascii characters only";
   }
   if (!validator.isLength(req.body.password, {min: 8, max: 25})){
-    errors["password" = "Minimum of 8 characters, max of 25";
+    errors["password"] = "Minimum of 8 characters, max of 25";
   }
 }
 
 exports.validateUser = function (errors, req){
   return new Promise(function(resolve, reject){
-    validateCreateUserFields(erros, req);
-    return model.User.findOne({
+    validateCreateUserFields(errors, req);
+    return models.User.findOne({
       where: {
         email: req.body.email
       }
